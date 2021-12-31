ddfe35fdb3875f54e33c04805ee9dbdf8cc5ca03
Mohammad Hossein Nourian <mhn_1991@yahoo.com>
2021-12-31 04:15:54 +0000

    final commit solver function is written

--- dutch_flag_problem.c
+++ dutch_flag_problem.c
@@ -14,7 +14,7 @@ void print_flag(int *flag){
 }
 
 
-int * fill_the_flag(int * flag){
+void fill_the_flag(int * flag){
   int color = 1;
   int counter   = 0;
   int *first = flag;
@@ -41,8 +41,40 @@ int * fill_the_flag(int * flag){
   print_flag(flag);
 }
 
+void solver(int *flag){
+  int array_size = 9;
+  int low_index  = 0;
+  int mid_index  = 0;
+  int high_index = array_size - 1;
+  int tmp = 0;
+  while(mid_index <= high_index){
+    switch(*(flag+mid_index)){
+    case Red:
+      tmp = *(flag+low_index);
+      *(flag+low_index) = Red;
+      *(flag+mid_index) = tmp;
+      low_index +=1;
+      mid_index +=1;
+      break;
+    case White:
+      mid_index +=1;
+      break;
+    case Blue:
+      tmp = *(flag+high_index);
+      *(flag+high_index) = Blue;
+      *(flag+mid_index) = tmp;
+      high_index -=1;
+      break;
+    }
+  }
+}
+
+
 int main(){
   int flag[9]= {0};
   //print_flag(flag);
   fill_the_flag(flag);
+  solver(flag);
+  print_flag(flag);
+  //printf("%d\n",*(&flag +1) - flag);
 }
